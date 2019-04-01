8207b0f5446e925f4fc4af64d0a696042dc1adb2
sonja <sonja@192.168.1.120>
2019-04-01 18:13:42 +0200

    Project Initialized and header finished

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
    
+   //Owl carousel
+   
+   if($('.owl-carousel').length > 0){
+       //lead-slider
+       $('.lead-slider').owlCarousel({
+           items:1,
+           nav:true,
+           dots:true,
+           loop:true,
+           navText:['<span class=""fa fa-ungle-left></span>','<span class=""fa fa-ungle-right></span>'],
+           autoplay:true
+       }); 
+       
+       
+   }
     
     
     
@@ -9,6 +24,6 @@ $(document).ready(function(){
     
     
     
-});
+});//document ready end
 
 
