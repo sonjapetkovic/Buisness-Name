1c4aaba9868af848e0735a793111503d56ef8cd8
sonja <sonja@192.168.1.120>
2019-04-08 15:22:14 +0200

    Header animated and easy scroll integrated.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,58 @@
 $(document).ready(function(){
    
+   //Header animation
+
+    function animateHeader() {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > 50) {
+            $('header').addClass('header-active  py-lg-2').removeClass(' py-lg-4');
+        } else {
+            $('header').removeClass('header-active  py-lg-2').addClass(' py-lg-4');
+        }
+    }
+   
+   //animation 
+    function animation() {
+
+        var windowHeight = $(window).height();
+        var scrollDown = $(window).scrollTop();
+
+        $('.animation').each(function () {
+            var position = $(this).offset().top;
+
+            if (position < scrollDown + windowHeight - 50) {
+                var animacija = $(this).attr('data-animation');
+                var delay = $(this).attr('data-delay');
+                $(this).css('animation-delay', delay);
+                $(this).addClass(animacija);
+                
+            }
+        });
+    }
+
+    animateHeader();
+    animation();
+
+    $(window).scroll(function () {
+        animateHeader();
+        animation();
+    });
+
+
+
+    // EASE SCROLL
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800);
+    });
+
+
+   
+   
    //Owl carousel
    
    if($('.owl-carousel').length > 0){
